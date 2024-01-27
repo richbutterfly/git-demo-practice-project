e15a80a225271a4ba68bbd6babd567a552f16060
richbutterfly <rich.butterfly04@gmail.com>
2024-01-27 15:14:09 +0000

    commit2

--- src/Test.java
+++ src/Test.java
@@ -1,6 +1,7 @@
 public class Test {
 
     public static void main(String[] args) {
+        System.out.println("My name is Khyati, Hello");
         System.out.println("My name is Khyati, How are you?");
     }
 }
