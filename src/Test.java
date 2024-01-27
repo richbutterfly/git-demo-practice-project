022c66d3339f0edb1aaf75e4b58f3ebcf675fb49
richbutterfly <147213005+richbutterfly@users.noreply.github.com>
2024-01-27 15:09:39 +0000

    commit1

--- src/Test.java
+++ src/Test.java
@@ -1,6 +1,6 @@
 public class Test {
 
     public static void main(String[] args) {
-        System.out.println("My name is Khyati");
+        System.out.println("My name is Khyati, How are you?");
     }
 }
